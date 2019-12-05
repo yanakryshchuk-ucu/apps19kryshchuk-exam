1e475ef94cdf67c060d7c45373557578a8bfcace
Yana Kryshchuk <kryshchuk@ucu.edu.ua>
2019-12-05 13:49:15 +0200

    Initial commit

---
+++ src/main/java/json/JsonBoolean.java
@@ -0,0 +1,19 @@
+package json;
+
+/**
+ * Created by Andrii_Rodionov on 1/4/2017.
+ */
+public class JsonBoolean extends Json {
+
+    private final Boolean bool;
+
+    public JsonBoolean(Boolean bool) {
+        this.bool = bool;
+    }
+
+    @Override
+    public String toJson() {
+        return String.valueOf(bool);
+    }
+}
+
